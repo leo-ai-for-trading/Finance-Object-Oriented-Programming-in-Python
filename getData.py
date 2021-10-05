a05c836adaa2e30dcc7e4bdb025461e7f6c5d97c
_Eliogabalo_ <70145737+leo-ai-for-trading@users.noreply.github.com>
2021-10-05 21:52:55 +0200

    Create getData.py
    Obtain n-data from yfinance library

---
+++ getData.py
@@ -0,0 +1,10 @@
+def getData(tickers,i):
+  #tickers is an empty list
+  #i: numbers of ticker 
+    for x in range(i):
+        ask = input('Insert a ticker: ')
+        tickers.append(ask)
+    data = yf.download(tickers, period='ytd',interval='1d',auto_adjust=True,
+    group_by='ticker')
+    print(data)
+    return data
